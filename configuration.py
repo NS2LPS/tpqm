c254dffccb40bbc0def994d0a8b47cc6a734dd8f
NS2 group at LPS <jerome.esteve@u-psud.fr>
2025-01-28 17:26:06 +0100

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -15,8 +15,7 @@ u = unit(coerce_to_integer=True)
 ######################
 # Network parameters #
 ######################
-from qm_ip import qop_ip
-cluster_name = "Cluster_1"  # Write your cluster_name if version >= QOP220
+from qm_ip import qop_ip, cluster_name
 qop_port = None  # Write the QOP port if version < QOP220
 
 #############################################
