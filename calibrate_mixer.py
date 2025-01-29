df77a0d2b096efbdf0b8f95a5611f496998ccd91
NS2 group at LPS <jerome.esteve@u-psud.fr>
2025-01-29 11:45:57 +0100

    Update calibrate_mixer.py

--- calibrate_mixer.py
+++ calibrate_mixer.py
@@ -1,9 +1,9 @@
 # Use 'run -i calibrate_mixer' to calibrate the mixer
 # Then, restart the QM with 'run -i reload' 
 
-from configuration import radar_LO, radar_IF, qm
+from configuration import rf1_LO, rf1_IF, qm
 
-caldict = {radar_LO: [radar_IF,]}
+caldict = {rf1_LO: [rf1_IF,]}
 
 print('Calibrating ...')
 qm.calibrate_element('radar',caldict)
